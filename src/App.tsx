f815e5d48c32ba37f7d74c26bd1f424f83ccb30a
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-05-18 00:30:23 +0300

    feat(App):add App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+import GlobalStyles from "./GlobalStyles";
+import AppRouter from "./router/AppRouter";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Container>
+          <AppRouter />
+        </Container>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
+
+const StyledApp = styled.div`
+  padding: 16px 0;
+  max-width: 1920px;
+  width: 100%;
+`;
+
+const Container = styled.div`
+  max-width: 1120px;
+  width: 100%;
+  margin: 0 auto;
+`;
