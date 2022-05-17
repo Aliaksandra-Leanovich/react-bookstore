0c3e75bbd8981a73748fc5e1440240802185f37b
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-05-18 00:35:16 +0300

    feat(services):add book services

---
+++ src/services/types/intex.ts
@@ -0,0 +1,43 @@
+export interface IBook {
+  title: string;
+  subtitle: string;
+  isbn13: string;
+  price: string;
+  image: string;
+  url: string;
+}
+
+export interface INewBooksApi {
+  books: IBook[];
+  error: string;
+  total: string;
+}
+export interface INewBookApi {
+  image: string;
+  isbn13: string;
+  price: string;
+  subtitle: string;
+  title: string;
+  url: string;
+}
+export interface IBookDetailsApi {
+  author: string;
+  desc: string;
+  error: string;
+  image: string;
+  isbn10: string;
+  isbn13: string;
+  language: string;
+  pages: string;
+  pdf: { [key: string]: string };
+  price: string;
+  publisher: string;
+  rating: string;
+  subtitle: string;
+  title: string;
+  url: string;
+  year: string;
+}
+export interface ISearchBookApi extends INewBooksApi {
+  page: string;
+}
