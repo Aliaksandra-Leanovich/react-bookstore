451980c544501d80a9e0c049dfeb235c3cdaf4a4
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-05-18 00:38:28 +0300

    feat(header):add form in header

---
+++ src/components/HeaderForm/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const StyledHeaderForm = styled.form`
+  max-width: 542px;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  border: 1px solid rgb(231, 231, 231);
+`;
+export { StyledHeaderForm };
