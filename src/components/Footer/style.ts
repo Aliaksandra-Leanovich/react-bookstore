241dc950341614b6ddb09f14013b3efc88dd67e2
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-05-18 00:37:26 +0300

    feat(footer):add footer and style to it

---
+++ src/components/Footer/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const StyledFooter = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin-top: 72px;
+  border-top: 1px solid rgb(231, 231, 231);
+`;
+export { StyledFooter };
