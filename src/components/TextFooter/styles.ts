0ae34e713aaa314c7b5a8019ae4ecd74e4b0c704
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-05-18 00:40:00 +0300

    feat(footer):add text to footer

---
+++ src/components/TextFooter/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const StyledTextFooter = styled.p`
+  padding-top: 35px;
+  padding-bottom: 35px;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  color: rgb(168, 168, 168);
+`;
+export { StyledTextFooter };
