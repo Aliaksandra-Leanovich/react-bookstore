18512f7cc2c44c29ec66821aae5766c8619318f9
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-05-23 00:47:46 +0300

    feat(account page): add Account page

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { routes } from "../routes/routes";
+import { RootStore } from "../store/store";
 
 export const Account = () => {
-  return <h1>Accpunt</h1>;
+  const { isAuthorized, email } = useSelector(({ user }: RootStore) => user);
+
+  if (isAuthorized) {
+    return <h1>{email}</h1>;
+  }
+  return <Navigate to={routes.SIGNUP} />;
 };
