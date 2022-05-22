f967736d303f7530ac508dc8dc661bad4ce108bf
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-05-23 00:56:28 +0300

    feat(app router):add main template

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { MainTemplate } from "../components/MainTemplate/MainTemplate";
 import { Account } from "../pages/Account";
 import { Cart } from "../pages/Cart";
 import { DetailsBook } from "../pages/DetailsBook";
@@ -15,16 +16,18 @@ import { routes } from "../routes/routes";
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path={routes.HOME} element={<Home />}></Route>
-      <Route path="/newbooks" element={<NewBooks />}></Route>
-      <Route path="/books/:id" element={<DetailsBook />}></Route>
-      <Route path="/search/:title/:page" element={<Search />}></Route>
-      <Route path="/account" element={<Account />}></Route>
-      <Route path={routes.CART} element={<Cart />}></Route>
-      <Route path="/favorites" element={<Favorites />}></Route>
-      <Route path={routes.SIGNIN} element={<Signin />}></Route>
-      <Route path={routes.SIGNUP} element={<Register />}></Route>
-      <Route path={routes.NOT_FOUND} element={<NotFound />}></Route>
+      <Route path="/" element={<MainTemplate />}>
+        <Route path={routes.HOME} element={<Home />}></Route>
+        <Route path="/newbooks" element={<NewBooks />}></Route>
+        <Route path="/books/:id" element={<DetailsBook />}></Route>
+        <Route path="/search/:title/:page" element={<Search />}></Route>
+        <Route path="/account" element={<Account />}></Route>
+        <Route path={routes.CART} element={<Cart />}></Route>
+        <Route path="/favorites" element={<Favorites />}></Route>
+        <Route path={routes.SIGNIN} element={<Signin />}></Route>
+        <Route path={routes.SIGNUP} element={<Register />}></Route>
+        <Route path={routes.NOT_FOUND} element={<NotFound />}></Route>
+      </Route>
     </Routes>
   );
 };
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,5 @@
 export enum routes {
-  HOME = "/",
+  HOME = "/react-bookstore",
   CART = "/cart",
   ACCOUNT = "/account",
   SIGNIN = "/sign-in",
