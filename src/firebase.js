fc0c431b4be174d2bf43109287508ac2484a3f96
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-05-23 00:29:41 +0300

    init(project):add firebase

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAZQvfjL-0EJRmSCx77V5m6odq1qUsgyLM",
+  authDomain: "booklist-c02e8.firebaseapp.com",
+  projectId: "booklist-c02e8",
+  storageBucket: "booklist-c02e8.appspot.com",
+  messagingSenderId: "176005663395",
+  appId: "1:176005663395:web:b811862de35dd6dcca8d16",
+};
+
+// Initialize Firebase
+// eslint-disable-next-line no-unused-vars
+const app = initializeApp(firebaseConfig);
