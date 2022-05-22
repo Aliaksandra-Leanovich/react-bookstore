31cfc094303852b9193ddb7df0f6159c2f0d9d01
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-05-23 00:54:24 +0300

    feat(app router):add pages

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { MainTemplate } from "../components/MainTemplate/MainTemplate";
 import { Account } from "../pages/Account";
 import { Cart } from "../pages/Cart";
 import { DetailsBook } from "../pages/DetailsBook";
 import { Favorites } from "../pages/Favorites";
 import { Home } from "../pages/Home";
+import { NewBooks } from "../pages/NewBooks";
+import { NotFound } from "../pages/NotFound";
+import { Register } from "../pages/Register";
 import { Search } from "../pages/Search";
+import { Signin } from "../pages/Signin";
 import { routes } from "../routes/routes";
 
 const AppRouter = () => {
