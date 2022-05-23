5a612e70431d7e29568ec08a542769b754237aea
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-05-24 02:04:14 +0300

    feat(sign in): change style

--- src/components/SignInForm/styles.ts
+++ src/components/SignInForm/styles.ts
@@ -4,32 +4,44 @@ import { Colors } from "../../ui/colors";
 const StyledSignInForm = styled.form`
   display: grid;
   row-gap: 20px;
+
   margin: 20px 0;
   width: 100%;
   max-width: 500px;
 `;
+
 const SignInInput = styled.input`
-  border: 2px solid ${Colors.BLACK};
-  color: ${Colors.BLACK};
   font-weight: 16px;
   line-height: 20px;
-  padding: 16px;
+
+  color: ${Colors.BLACK};
+
+  border: 2px solid ${Colors.BLACK};
   border-radius: 20px;
+
   width: 100%;
+  padding: 16px;
+
   &::placeholder {
     color: ${Colors.GREY};
   }
 `;
+
 const SignInButton = styled.button`
-  cursor: pointer;
-  border: 2px solid ${Colors.BLACK};
-  color: ${Colors.BLACK};
-  background-color: ${Colors.ORANGE};
   font-size: 16px;
   font-weight: 500;
   line-height: 20px;
+
+  color: ${Colors.BLACK};
+
   padding: 10px;
-  border-radius: 25px;
   width: 100%;
+
+  border-radius: 25px;
+  border: 2px solid ${Colors.BLACK};
+
+  background-color: ${Colors.ORANGE};
+
+  cursor: pointer;
 `;
 export { StyledSignInForm, SignInInput, SignInButton };
