92c8f8d220cd8e38aeb1366632c9a289c0119549
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-05-24 01:57:36 +0300

    feat(footer):change in style

--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -4,8 +4,10 @@ import { Colors } from "../../ui/colors";
 const StyledFooter = styled.div`
   display: flex;
   justify-content: space-between;
+
   width: 100%;
   margin-top: 72px;
+
   border-top: 1px solid ${Colors.GREY};
 `;
 export { StyledFooter };
