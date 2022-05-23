73fb4dc91aeb8c3f1ec3422018ff72cf430d2cbf
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-05-24 02:06:09 +0300

    feat(sign up): change style

--- src/components/SignUpForm/styles.ts
+++ src/components/SignUpForm/styles.ts
@@ -4,32 +4,43 @@ import { Colors } from "../../ui/colors";
 const StyledSignUpForm = styled.form`
   display: grid;
   row-gap: 20px;
+
   margin: 20px 0;
   width: 100%;
   max-width: 500px;
 `;
+
 const SignUpInput = styled.input`
-  border: 2px solid ${Colors.BLACK};
-  color: ${Colors.BLACK};
   font-weight: 16px;
   line-height: 20px;
+
+  color: ${Colors.BLACK};
+
   padding: 16px;
-  border-radius: 20px;
   width: 100%;
+
+  border-radius: 20px;
+  border: 2px solid ${Colors.BLACK};
+
   &::placeholder {
     color: ${Colors.GREY};
   }
 `;
+
 const SignUpButton = styled.button`
-  cursor: pointer;
-  border: 2px solid ${Colors.BLACK};
-  color: ${Colors.BLACK};
-  background-color: ${Colors.YELLOW};
   font-size: 16px;
   font-weight: 500;
   line-height: 20px;
-  padding: 10px;
-  border-radius: 25px;
+
+  color: ${Colors.BLACK};
+  background-color: ${Colors.YELLOW};
+
   width: 100%;
+  padding: 10px;
+
+  border-radius: 25px;
+  border: 2px solid ${Colors.BLACK};
+
+  cursor: pointer;
 `;
 export { StyledSignUpForm, SignUpButton, SignUpInput };
