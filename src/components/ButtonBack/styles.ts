ee92689edf0720bc910f6b6aaea2fec6093c2e5b
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-05-24 01:56:51 +0300

    feat(button back): change style

--- src/components/ButtonBack/styles.ts
+++ src/components/ButtonBack/styles.ts
@@ -2,8 +2,15 @@ import styled from "styled-components";
 import { Colors } from "../../ui/colors";
 
 export const StyledButtonBack = styled.button`
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 18px;
+
+  padding: 4px 16px;
+
   background-color: ${Colors.BLUE};
+  color: ${Colors.BLACK};
+
   border: 2px solid ${Colors.BLACK};
-  padding: 4px 7px;
   border-radius: 15px;
 `;
