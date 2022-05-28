24e17c3cf4c84fd447ba78d0e3a9f9b7f410b01c
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-05-29 02:55:07 +0300

    feat(assets): add images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,8 +3,21 @@ import { ReactComponent as Favorites } from "./favorites.svg";
 import { ReactComponent as Logo } from "./logo.svg";
 import { ReactComponent as Account } from "./account.svg";
 import { ReactComponent as Heart } from "./heart.svg";
-import { ReactComponent as Star } from "./star-filled.svg";
+import { ReactComponent as FilledStar } from "./star-filled.svg";
+import { ReactComponent as NoFilledStar } from "./star-notfilled.svg";
 import { ReactComponent as Down } from "./down.svg";
+import { ReactComponent as Back } from "./arrow-left-long-solid.svg";
 import { ReactComponent as Trash } from "./trash.svg";
 
-export { Cart, Favorites, Logo, Account, Heart, Star, Down, Trash };
+export {
+  Cart,
+  Favorites,
+  Logo,
+  Account,
+  Heart,
+  FilledStar,
+  NoFilledStar,
+  Down,
+  Trash,
+  Back,
+};
