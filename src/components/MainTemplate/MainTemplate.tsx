799d40401c1278c8d52fd05addbe6985b4a67148
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-05-29 03:00:59 +0300

    feat(main template): change style

--- src/components/MainTemplate/MainTemplate.tsx
+++ src/components/MainTemplate/MainTemplate.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import { Header } from "../Header/Header";
+import { Container, StyledMainTeplate } from "./style";
 
 export const MainTemplate = () => {
   return (
-    <>
-      <Header></Header>
-      <Outlet />
-      <Footer></Footer>
-    </>
+    <StyledMainTeplate>
+      <Container>
+        <Header />
+        <Outlet />
+        <Footer />
+      </Container>
+    </StyledMainTeplate>
   );
 };
