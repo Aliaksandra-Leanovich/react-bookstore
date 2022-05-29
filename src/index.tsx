8e42d7d59505b426a8a4e5aa80d2a90adfc747bd
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-05-29 03:08:31 +0300

    feat(index.tsx): change components

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import store from "./store/store";
 import "./firebase";
+import GlobalStyles from "./GlobalStyles";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <GlobalStyles />
+      <App />
+    </Provider>
   </React.StrictMode>
 );
