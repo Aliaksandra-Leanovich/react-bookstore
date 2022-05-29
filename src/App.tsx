354c39d9d1cc1a8127b9dfbc2d504d4f311cdac7
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-05-29 03:07:44 +0300

    feat(app): change components

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
 import React from "react";
-import styled from "styled-components";
-import GlobalStyles from "./GlobalStyles";
+import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
-    <>
-      <GlobalStyles />
-      <StyledApp>
-        <Container>
-          <AppRouter />
-        </Container>
-      </StyledApp>
-    </>
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   );
 }
 
 export default App;
-
-const StyledApp = styled.div`
-  padding: 16px 0;
-  max-width: 1920px;
-  width: 100%;
-`;
-
-const Container = styled.div`
-  max-width: 1120px;
-  width: 100%;
-  margin: 0 auto;
-`;
