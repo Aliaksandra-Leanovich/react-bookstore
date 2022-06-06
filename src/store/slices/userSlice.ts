133a841941ef4e0af3ebce4cd394faffc3c29cf9
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-06-07 01:32:25 +0300

    feat(app):changes in settings

--- src/store/slices/userSlice.ts
+++ src/store/slices/userSlice.ts
@@ -10,7 +10,7 @@ interface IUserStore {
 }
 
 const initialState: IUserStore = {
-  isAuthorized: true, //change!
+  isAuthorized: false, //change!
   email: undefined,
   name: undefined,
   favorites: [],
