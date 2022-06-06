2ad945228833123cf43a880639edd09da87ba905
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-06-07 01:20:49 +0300

    feat(favorites):add style

--- src/components/ItemFavorites/ItemFavorites.tsx
+++ src/components/ItemFavorites/ItemFavorites.tsx
@@ -13,6 +13,7 @@ import {
   InfoContainer,
   StyledLink,
   TrashContainer,
+  Container,
 } from "./style";
 
 interface IBook {
@@ -25,22 +26,24 @@ export const ItemFavorites = ({ book }: IBook) => {
     dispatch(deleteFavorite(book));
   };
   return (
-    <StyledItemFavorites>
-      <StyledLink to={`/books/${book.isbn13}`}>
-        <BookImage src={book.image} alt={book.title} />
-        <InfoContainer>
-          <BookTitle>{book.title}</BookTitle>
-          <BookSubtitle>{book.subtitle}</BookSubtitle>
-        </InfoContainer>
-        <AdditionalContainer>
-          <BookPrice>
-            {book.price === "$0.00" ? "Currently not available" : book.price}
-          </BookPrice>
-        </AdditionalContainer>
-      </StyledLink>
+    <Container>
+      <StyledItemFavorites>
+        <StyledLink to={`/books/${book.isbn13}`}>
+          <BookImage src={book.image} alt={book.title} />
+          <InfoContainer>
+            <BookTitle>{book.title}</BookTitle>
+            <BookSubtitle>{book.subtitle}</BookSubtitle>
+          </InfoContainer>
+          <AdditionalContainer>
+            <BookPrice>
+              {book.price === "$0.00" ? "Currently not available" : book.price}
+            </BookPrice>
+          </AdditionalContainer>
+        </StyledLink>
+      </StyledItemFavorites>
       <TrashContainer onClick={() => handleFavorite(book)}>
         <Trash />
       </TrashContainer>
-    </StyledItemFavorites>
+    </Container>
   );
 };
