e92cb74b4ceca2f83c6c923010e98d97a00254a7
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-06-06 15:20:19 +0300

    feat(acount): add profile and button back

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,19 +1,27 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Navigate, useNavigate } from "react-router-dom";
+import { ButtonBack } from "../components/ButtonBack/ButtonBack";
+import { Profile } from "../components/Profile/Profile";
 import { routes } from "../routes/routes";
-import { useAppSelector } from "../store/hooks/hooks";
-import { getUserInfo } from "../store/selectors/userSelector";
+import { RootState } from "../store/store";
 
 export const Account = () => {
-  const { isAuthorized, email, name } = useAppSelector(getUserInfo);
-  // const { isAuthorized, email } = useSelector(({ user }: RootState) => user);
+  const { isAuthorized } = useSelector(({ user }: RootState) => user);
+
+  const navigate = useNavigate();
+  const handleBack = () => {
+    navigate(-1);
+  };
 
   if (isAuthorized) {
     return (
-      <div>
-        <h1>{email}</h1>
-        <h2>{name}</h2>
-      </div>
+      <>
+        <ButtonBack type="button" handleBack={handleBack}>
+          Back
+        </ButtonBack>
+        <Profile />
+      </>
     );
   }
   return <Navigate to={routes.SIGNUP} />;
